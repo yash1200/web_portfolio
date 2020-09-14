55036ddd0d7f9a7e2c4570f2ecc4e600fcafa71f
Yash Johri <yashjohri1200@gmail.com>
2020-09-14 21:34:59 +0530

    About me updated

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -9,10 +9,10 @@ final String about =
     "I'm Yash Johri, currently persuing Btech. in Computer Engineering"
     " from Jamia Millia Islamia, New Delhi. I'm a Flutter and Android  Developer"
     ", with a keen understanding of Dart, Java, Firebase, Cloud Firestore,"
-    " Data Structures and Algorithms.\n\nI'm a 1500+ Codeforces rated coder and a 4 star coder on Codechef. I "
-    "qualified for regional level in ACM ICPC, and have also participated"
-    " in Google Hashcode and Google Kickstart. Other than coding, my interests "
-    "lie in Cricket, Soduko, Sci-fi movies & exploring latest technology "
+    " Data Structures and Algorithms.\n\nI'm a 1500+ Codeforces rated coder and a 4 star coder on Codechef, "
+    "qualified for regional level in ACM ICPC, and also contributed"
+    " in Open Source Flutter repository. Other than coding, my interests "
+    "lie in Cricket, Soduko, Anime, Sci-fi movies & exploring latest technological "
     "advancements in the developing world.";
 
 final String experience = 'The only source of knowledge is experience';
