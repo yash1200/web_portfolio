a09bc20c7f0d74b509345efade05942055f4c71f
Yash Johri <yashjohri1200@gmail.com>
2020-10-01 14:33:03 +0530

    Removed unneeded files

--- android/app/src/main/kotlin/com/example/resume_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.resume_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/web_portfolio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.web_portfolio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
