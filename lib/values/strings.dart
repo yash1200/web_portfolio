97b7a34aa173c2b903a14ede1a7004ebd63a96f2
Yash Johri <yashjohri1200@gmail.com>
2020-10-14 18:23:36 +0530

    About me fixed

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -6,9 +6,9 @@ final String resumeUrl =
     'https://drive.google.com/file/d/1wAqf5A8C9oPQ4bYpazWG81626BC-lXNq/view?usp=sharing';
 final String githubRepoUrl = 'https://github.com/yash1200?tab=repositories';
 final String about =
-    "I'm Yash Johri, currently persuing Btech. in Computer Engineering"
+    "I'm Yash Johri, currently pursuing Btech. in Computer Engineering"
     " from Jamia Millia Islamia, New Delhi. I'm a Full Stack Mobile Developer"
-    ", with a keen understanding of Flutter, Android, NodeJs, MongoDB,"
+    ", with a keen understanding of Flutter, Android, VueJs, NodeJs, MongoDB,"
     " Data Structures and Algorithms.\n\nI'm a 1500+ Codeforces rated coder and a 4 star coder on Codechef, "
     "qualified for regional level in ACM ICPC, and also contributed"
     " in Open Source Flutter repository. Other than coding, my interests "
